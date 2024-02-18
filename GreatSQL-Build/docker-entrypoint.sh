a249eeeabc9fda9ca2a2898a857cf01a5e7845ea
Ye Jinrong <jinrong.ye@greatdb.com>
2024-02-18 17:44:56 +0800

    更新：编译完成后，删除临时文件目录，只保留源码包

--- GreatSQL-Build/docker-entrypoint.sh
+++ GreatSQL-Build/docker-entrypoint.sh
@@ -13,5 +13,6 @@ echo "2. entering greatsql automake" ; \
 su - mysql -s /bin/bash -c "cd /opt; /bin/sh /opt/greatsql-automake.sh" && \
 echo "3. greatsql automake completed" ; \
 ls -la ${OPT_DIR} | grep GreatSQL.*glibc.* && ${OPT_DIR}/GreatSQL*glibc*/bin/mysqld --verbose --version && \
-echo "4. entering /bin/bash" ;\
+echo "4. remove files and clean up" ;\
+cd ${OPT_DIR} && rm -rf boost_1_77_0 greatsql-8.0.32-25 patchelf-0.14.5
 /bin/bash
