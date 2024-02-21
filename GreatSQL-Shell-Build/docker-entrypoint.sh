8ce557eba10e99fdf7903b6b86fc1b7213163503
Ye Jinrong <jinrong.ye@greatdb.com>
2024-02-21 09:36:47 +0800

    新增GreatSQL-Shell编译相关脚本

---
+++ GreatSQL-Shell-Build/docker-entrypoint.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+. /opt/greatsqlsh-setenv.sh
+
+echo "0. touch logfile ${MAKELOG}"
+touch ${MAKELOG} && \
+chown ${MYSQL_USER}:${MYSQL_USER} ${MAKELOG} && \
+chmod 0777 ${MAKELOG} && \
+echo && \
+echo "1. downloading sourcecode tarballs and extract"
+cd ${OPT_DIR} && \
+echo "downloading sourcecode tarballs ..." && \
+wget -c -O ${GREATSQLSH_ENV} ${GREATSQLSH_BUILD_DOWNLOAD_URL}/${GREATSQLSH_ENV} >> ${MAKELOG} 2>&1 && \
+wget -c -O ${GREATSQLSH_MAKESH} ${GREATSQLSH_BUILD_DOWNLOAD_URL}/${GREATSQLSH_MAKESH} >> ${MAKELOG} 2>&1 && \
+wget -c -O ${GREATSQLSH_PATCH} ${GREATSQLSH_BUILD_DOWNLOAD_URL}/${GREATSQLSH_PATCH} >> ${MAKELOG} 2>&1 && \
+wget -c -O ${RPCGEN} ${GREATSQL_BUILD_DOWNLOAD_URL}/${RPCGEN} >> ${MAKELOG} 2>&1 && \
+wget -c -O ${PATCHELF}.tar.xz ${GREATSQL_BUILD_DOWNLOAD_URL}/${PATCHELF}.tar.xz >> ${MAKELOG} 2>&1 && \
+wget -c -O ${PROTOBUF}.tar.xz ${GREATSQL_BUILD_DOWNLOAD_URL}/${PROTOBUF}.tar.xz >> ${MAKELOG} 2>&1 && \
+wget -c -O ${ANTLR}.tar.xz ${GREATSQL_BUILD_DOWNLOAD_URL}/${ANTLR}.tar.xz >> ${MAKELOG} 2>&1 && \
+wget -c -O ${BOOST}.tar.gz ${BOOST_SRC_DOWNLOAD_URL}/${BOOST}.tar.gz >> ${MAKELOG} 2>&1 && \
+wget -c -O ${MYSQLSH}.tar.gz ${MYSQLSH_SRC_DOWNLOAD_URL}/${MYSQLSH}.tar.gz >> ${MAKELOG} 2>&1 && \
+wget -c -O ${MYSQL}.tar.gz ${MYSQL_SRC_DOWNLOAD_URL}/${MYSQL}.tar.gz >> ${MAKELOG} 2>&1 && \
+echo "extract tarballs ..." && \
+tar xf ${OPT_DIR}/${PATCHELF}*z && \
+tar xf ${OPT_DIR}/${PROTOBUF}*z && \
+tar xf ${OPT_DIR}/${ANTLR}*z && \
+tar xf ${OPT_DIR}/${BOOST}*z && \
+tar xf ${OPT_DIR}/${MYSQL}*z && \
+tar xf ${OPT_DIR}/${MYSQLSH}*z && \
+chown -R ${MYSQL_USER}:${MYSQL_USER} ${OPT_DIR} && \
+dnf install -y ${RPCGEN} >> ${MAKELOG} 2>&1 && \
+chmod +x ${OPT_DIR}/*sh && \
+echo && \
+echo "2. compiling antlr4"
+cd ${OPT_DIR}/${ANTLR}/runtime/Cpp/bld && \
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/antlr4 >> ${MAKELOG} 2>&1 && \
+make -j${MAKE_JOBS} >> ${MAKELOG} 2>&1 && make -j${MAKE_JOBS} install >> ${MAKELOG} 2>&1 && \
+echo && \
+echo "3. compiling patchelf"
+cd ${OPT_DIR}/${PATCHELF} && \
+./bootstrap.sh >> ${MAKELOG} 2>&1 && \
+./configure >> ${MAKELOG} 2>&1 && \
+make -j${MAKE_JOBS} >> ${MAKELOG} 2>&1 && \
+make -j${MAKE_JOBS} install >> ${MAKELOG} 2>&1 && \
+echo && \
+echo "4. compiling protobuf"
+cd ${OPT_DIR}/${PROTOBUF} && \
+./configure >> ${MAKELOG} 2>&1 && \
+make -j${MAKE_JOBS} >> ${MAKELOG} 2>&1 && \
+make -j${MAKE_JOBS} install >> ${MAKELOG} 2>&1 && \
+echo && \
+echo "5. compiling MySQL Shell for GreatSQL"
+su - ${MYSQL_USER} -s /bin/bash -c "cd ${OPT_DIR}; /bin/sh ${OPT_DIR}/${GREATSQLSH_MAKESH}" && \
+echo && \
+echo "6. MySQL Shell for GreatSQL 8.0.32-25 build completed! TARBALL file: "
+pip3.8 install --user certifi pyclamd >> ${MAKELOG} 2>&1 && \
+${BASE_DIR}/bin/mysqlsh --version && \
+cd ${OPT_DIR} && \
+tar cf ${GREATSQLSH}.tar ${GREATSQLSH} >> ${MAKELOG} 2>&1 && \
+xz -9 -f -T${MAKE_JOBS} ${GREATSQLSH}.tar >> ${MAKELOG} 2>&1 && \
+ls -la ${GREATSQLSH}.tar.xz && \
+cd ${OPT_DIR} && \
+rm -fr ${ANTLR}* ${BOOST}* ${MYSQL}* ${MYSQLSH}* ${MYSQLSH_PATCH} ${PATCHELF}* ${PROTOBUF}* ${RPCGEN}
+/bin/bash
---
+++ GreatSQL-Shell-Build/greatsqlsh-automake.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+. /opt/greatsqlsh-setenv.sh
+
+echo "5.1 compiling mysqlclient and mysqlxclient" && \
+cd ${OPT_DIR}/${MYSQL} && \
+rm -fr bld && \
+mkdir bld && \
+cd bld && \
+cmake .. -DBOOST_INCLUDE_DIR=${OPT_DIR}/${BOOST} \
+-DLOCAL_BOOST_DIR=${OPT_DIR}/${BOOST} \
+-DWITH_SSL=system >> ${MAKELOG} 2>&1 && \
+cmake --build . --target mysqlclient -- -j${MAKE_JOBS} >> ${MAKELOG} 2>&1 ; \
+cmake --build . --target mysqlxclient -- -j${MAKE_JOBS} >> ${MAKELOG} 2>&1 && \
+echo && \
+echo "5.2 compiling MySQL Shell for GreatSQL" && \
+cd ${OPT_DIR}/${MYSQLSH} && \
+patch -p1 -f < ${OPT_DIR}/${GREATSQLSH_PATCH} >> ${MAKELOG} 2>&1 && \
+rm -fr bld && \
+mkdir bld && \
+cd bld && \
+cmake .. \
+-DCMAKE_INSTALL_PREFIX=${BASE_DIR} \
+-DMYSQL_SOURCE_DIR=${OPT_DIR}/${MYSQL} \
+-DMYSQL_BUILD_DIR=${OPT_DIR}/${MYSQL}/bld/ \
+-DHAVE_PYTHON=1 \
+-DWITH_PROTOBUF=bundled \
+-DBUILD_SOURCE_PACKAGE=0 \
+-DBUNDLED_ANTLR_DIR=/usr/local/antlr4/ \
+-DPYTHON_LIBRARIES=/usr/lib64/python3.8 -DPYTHON_INCLUDE_DIRS=/usr/include/python3.8/ >> ${MAKELOG} 2>&1 \
+&& make -j${MAKE_JOBS} >> ${MAKELOG} 2>&1 && make -j${MAKE_JOBS} install >> ${MAKELOG} 2>&1 && \
+cp /usr/local/lib/libprotobuf.so.30 ${BASE_DIR}/lib/mysqlsh/
