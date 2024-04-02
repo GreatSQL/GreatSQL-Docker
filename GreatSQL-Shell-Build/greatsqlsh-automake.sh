de25f988cd88fc27bb329cada025301ef3e83a25
Ye Jinrong <jinrong.ye@greatdb.com>
2024-04-02 09:20:13 +0800

    将更多依赖动态库文件CP到发行包中，方便在其他平台直接运行

--- GreatSQL-Shell-Build/greatsqlsh-automake.sh
+++ GreatSQL-Shell-Build/greatsqlsh-automake.sh
@@ -32,8 +32,35 @@ cmake .. \
 make -j${MAKE_JOBS} >> ${MAKELOG} 2>&1 && \
 make -j${MAKE_JOBS} install >> ${MAKELOG} 2>&1 && \
 cp /usr/local/lib/libprotobuf.so.30 ${BASE_DIR}/lib/mysqlsh/ &&
-cp /lib64/libnode.so.93 ${BASE_DIR}/lib/mysqlsh/libnode.so.93 && \
+cp /lib64/libnode.so.93 ${BASE_DIR}/lib/mysqlsh/ && \
 cp /lib64/libbrotlienc.so.1.0.6 ${BASE_DIR}/lib/mysqlsh/libbrotlienc.so.1 && \
 cp /lib64/libbrotlidec.so.1.0.6 ${BASE_DIR}/lib/mysqlsh/libbrotlidec.so.1 && \
 cp /lib64/libuv.so.1.0.0 ${BASE_DIR}/lib/mysqlsh/libuv.so.1 && \
-cp /lib64/libbrotlicommon.so.1.0.6 ${BASE_DIR}/lib/mysqlsh/libbrotlicommon.so.1
+cp /lib64/libbrotlicommon.so.1.0.6 ${BASE_DIR}/lib/mysqlsh/libbrotlicommon.so.1 && \
+cp /lib64/libssl.so.1.1.1k ${BASE_DIR}/lib/mysqlsh/libssl.so.1.1 && \
+cp /lib64/libcrypto.so.1.1.1k ${BASE_DIR}/lib/mysqlsh/libcrypto.so.1.1 && \
+cp /lib64/libcrypt.so.1.1.0 ${BASE_DIR}/lib/mysqlsh/libcrypt.so.1.1 && \
+cp /lib64/libssh.so.4.8.5 ${BASE_DIR}/lib/mysqlsh/libssh.so.4 && \
+cp /lib64/libpython3.8.so.1.0 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libnghttp2.so.14.19.0 ${BASE_DIR}/lib/mysqlsh/libnghttp2.so.14 && \
+cp /lib64/libpcre2-8.so.0.7.1 ${BASE_DIR}/lib/mysqlsh/libpcre2-8.so.0 && \
+cp /lib64/libpthread.so.0 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libdl.so.2 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libresolv.so.2 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/librt.so.1 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libutil.so.1 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libcurl.so.4 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libm.so.6 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libstdc++.so.6 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libgcc_s.so.1 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libc.so.6 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libz.so.1 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libgssapi_krb5.so.2 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libkrb5.so.3 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libk5crypto.so.3 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libcom_err.so.2 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libcrypt.so.1 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libuuid.so.1 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libkrb5support.so.0 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libkeyutils.so.1 ${BASE_DIR}/lib/mysqlsh/ && \
+cp /lib64/libselinux.so.1 ${BASE_DIR}/lib/mysqlsh/
